8fd8988bc3a28e04ce8ae5084bb5cfc2fac60923
shawn <1131350950@qq.com>
2022-06-10 19:12:08 +0800

    add:FE page

--- main.go
+++ main.go
@@ -27,8 +27,14 @@ type User struct {
 	Res  string
 }
 
+var (
+	user       User
+	filesuffix string
+	mlName     = "dog.py"
+)
+
 func main() {
-	// 连接数据库
+	// ---------------------- 连接数据库 -------------------------------
 	db, err := gorm.Open(sqlite.Open("./db/project_ddd.db"))
 	if err != nil {
 		log.Fatal("open db failed!")
@@ -37,9 +43,7 @@ func main() {
 	// 对应
 	db.AutoMigrate(&User{})
 
-	// 创建
-
-	// ---------------------------------------------------------------
+	// ------------------------静态文件----------------------------------
 	r := gin.Default()
 
 	r.Static("/static", "./statics")
@@ -47,10 +51,11 @@ func main() {
 	r.Static("/img", "./statics/img")
 	r.Static("/js", "./statics/js")
 	r.Static("/fonts", "./statics/fonts")
+	r.Static("/images/", "./images/")
 
 	r.LoadHTMLGlob("./templates/*")
 
-	// ---------------------------------------------------------------
+	// -----------------------路由---------------------------------
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
@@ -59,25 +64,26 @@ func main() {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
-	r.GET("/about.html", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "about.html", nil)
-	})
-
-	r.GET("/about", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "about.html", nil)
-	})
-
 	r.GET("/service.html", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "service.html", nil)
 	})
 
+	r.GET("/about.html", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "about.html", nil)
+	})
+
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusNotFound, "404.html", nil)
+	})
+
 	// ---------------- 查询----------------------
 
-	// -----------------取结果---------------------
 	r.POST("/service.html", func(c *gin.Context) {
-		name := c.PostForm("name")
-		if name != "" {
+		userName := c.PostForm("name")
+		if userName != "" {
+			// === 一般查询 ===
 			age := c.PostForm("age")
+			agee, _ := strconv.Atoi(age)
 
 			file, err := c.FormFile("f1")
 			if err != nil {
@@ -86,13 +92,13 @@ func main() {
 				})
 				return
 			}
-
-			// 判断是不是图片，最简单的做法，最好的做法是判断文件前面的字节
+			// 判断文件：是不是图片格式
 			filename := file.Filename
-			filesuffix := path.Ext(filename)
+			filesuffix = path.Ext(filename)
 			if filesuffix != ".jpg" && filesuffix != ".png" && filesuffix != ".jpeg" {
-				c.JSON(http.StatusOK, gin.H{
-					"文件类型错误": "请上传后缀为jpeg/jpg/png的文件",
+				c.HTML(http.StatusOK, "error.html", gin.H{
+					"Error":     "文件类型错误",
+					"whatError": "请上传后缀为jpeg/jpg/png的文件",
 				})
 				return
 			}
@@ -100,75 +106,44 @@ func main() {
 			// 重命名文件
 			id := strings.TrimSuffix(getUUID(), "\n")
 			newFileName := id + filesuffix
+
+			// 上传文件 -> 指定的目录
 			dst := "./images/" + newFileName
-
-			// 上传文件到指定的目录
 			c.SaveUploadedFile(file, dst)
-			c.HTML(http.StatusOK, "res2.html", gin.H{
-				"ID": id,
-			})
 
-			// 将信息写入数据库
-			agee, _ := strconv.Atoi(age)
-
-			// ---------------同步：得到python结果--------------------------
-			type output struct {
-				out []byte
-				err error
-			}
-
-			ch := make(chan output)
-
-			go func() {
-				cmd := exec.Command("python", "dog.py", dst)
-				out, err := cmd.CombinedOutput()
-				ch <- output{out, err}
-			}()
-
-			select {
-			case <-time.After(5 * time.Second):
-				fmt.Println("timed out")
-			case x := <-ch:
-				user := User{Name: name, Age: uint8(agee), ID: id, Res: string(x.out)}
-				if x.err != nil {
-					fmt.Printf("python exec errored: %s\n", x.err)
-				}
-				if err := db.Create(&user).Error; err != nil {
-					log.Fatalf("插入数据失败：%v", err)
-				}
-			}
-
-			// -----------------------------------------------
-
+			// 计算 & 插入数据库
+			insertUser(agee, dst, id, userName, db)
 		} else {
+			// === 历史结果查询 ===
 			uuid := c.PostForm("uuid")
 
-			// 如果查不到，就return
-			fmt.Println("uuid:", uuid)
-			var user User
+			// 根据uuid查询
 			if err := db.First(&user, "id = ?", uuid).Error; err != nil {
-				c.JSON(http.StatusOK, gin.H{
-					"错误": "请输入正确的凭证码",
+				c.HTML(http.StatusOK, "error.html", gin.H{
+					"Error":     "凭证码错误",
+					"whatError": "请输入正确的凭证码",
 				})
 				return
 			}
 
-			time := user.CreatedAt
-			name := user.Name
-			age := user.Age
-			res := user.Res
-
-			c.HTML(http.StatusOK, "res.html", gin.H{
-				"Time": time,
-				"Name": name,
-				"Age":  age,
-				"ID":   uuid,
-				"Res":  res,
-			})
 		}
+
+		name := user.Name
+		time := user.CreatedAt
+		uuid := user.ID
+		res := user.Res
+		img := "images/" + user.ID + filesuffix
+
+		c.HTML(http.StatusOK, "res.html", gin.H{
+			"Name":  name,
+			"Time":  time.Format("2006/01/02 15:04"),
+			"ID":    uuid,
+			"Res":   res,
+			"image": img,
+		})
 	})
 
-	r.Run()
+	r.Run(":8080")
 }
 
 // 使用linux命令生成uuid
@@ -181,11 +156,31 @@ func getUUID() string {
 	return string(out)
 }
 
-func getRes(dst string) string {
-	out, err := exec.Command("python dog.py " + dst).Output()
-	if err != nil {
-		log.Fatal("res get failed! err:", err)
+func insertUser(agee int, dst string, id string, userName string, db *gorm.DB) {
+	type output struct {
+		out []byte
+		err error
 	}
 
-	return string(out)
+	ch := make(chan output)
+
+	go func() {
+		cmd := exec.Command("python", mlName, dst)
+		out, err := cmd.CombinedOutput()
+		ch <- output{out, err}
+	}()
+
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Println("timed out")
+	case x := <-ch:
+		user = User{Name: userName, Age: uint8(agee), ID: id, Res: string(x.out)}
+		if x.err != nil {
+			log.Fatalf("python exec errored: %s\n", x.err)
+		}
+		if err := db.Create(&user).Error; err != nil {
+			log.Fatalf("插入数据失败：%v", err)
+		}
+	}
+
 }
